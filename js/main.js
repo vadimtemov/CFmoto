6823eac5a2d26bc3c48bd6cf30aa209b468b00cd
vadimtemov <128626319+vadimtemov@users.noreply.github.com>
2023-10-05 21:46:24 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -218,3 +218,25 @@ const swiper = new Swiper('.route__slider', {
       },
     }
   });
+
+  // ------------------ Accordion ----------------------
+
+  const accordionLists = document.querySelectorAll('.accordion-list');
+
+  accordionLists.forEach(el => {
+    el.addEventListener('click', (e) => {
+
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if(!accordionControl) return
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+
+      accordionItem.classList.toggle('accordion-list__item--opened');
+
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
+    });
+  });
